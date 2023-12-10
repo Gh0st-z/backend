9efc23ff6d0f58d91b91130174ae31e7cd0e2990
Gh0st-z <shinigamikami446@gmail.com>
2023-12-10 11:51:10 +0545

    Api added

--- autho/views.py
+++ autho/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.hashers import check_password
+from .models import customers
 
-# Create your views here.
+@api_view(['POST'])
+def login_view(request):
+    if request.method == 'POST':
+        data = request.POST
+        email = data['email']
+        passwrd = data['password']
+
+        validating_user = customers.objects.get(email = email)
+        if check_password(passwrd, validating_user.password):
+            return redirect('home')
