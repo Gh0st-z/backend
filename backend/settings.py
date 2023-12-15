9ff6408da2729b5c56d5bcd18274b86c48423d20
Gh0st-z <shinigamikami446@gmail.com>
2023-12-15 11:28:47 +0545

    new migrations and fixed urls

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'autho',
+    'autho.apps.AuthoConfig',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.apps import autho
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(autho.urls)),
+    path('autho/', include('autho.urls'))
 ]
