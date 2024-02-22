1d5cf31b5528c048faa2b23646d29210a138129b
Gh0st-z <shinigamikami446@gmail.com>
2024-02-22 13:25:46 +0545

    created a post view for adding pharmacy and added the url

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from pharmacy.views import *
 
 urlpatterns = [
+    path('add-pharmacy/', PharmacyProfilePost, name='add-pharmacy'),
 ]
--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Pharmacy
+from .serializers import PharmacySerializer
 
-# Create your views here.
+@api_view(['POST'])
+def PharmacyProfilePost(request, *args, **kwargs):
+    data = {
+        'pharmacy_name': request.data.get('pharmacy_name'),
+        'address': request.data.get('address'),
+        'license_number': request.data.get('license_number'),
+        'phone_number': request.data.get('phone_number'),
+        'pharmacy_type': request.data.get('pharmacy_type'),
+        'pharmacy_logo': request.data.get('pharmacy_logo'),
+        'website_url': request.data.get('website_url'), 
+    }
+    serializer = PharmacySerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response({'message': 'Invalid data provided!'} ,status=status.HTTP_401_UNAUTHORIZED)
